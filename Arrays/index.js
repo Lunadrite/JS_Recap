6887dacb9b71105095a028796338e335eda2e3ee
Luna <lunatattersall@gmail.com>
2026-05-27 12:49:46 +0100

    array learnin

--- Arrays/index.js
+++ Arrays/index.js
@@ -27,10 +27,20 @@ while(numOfFruits != -1){
 }
     */
 
-// ---------------------        SPREAD OPERATORS- alows an iterable such as an array or string to be expanded [ ... ]
+// ---------------------        SPREAD OPERATORS- alows an iterable such as an array or string to be expanded [ ... ] (unpacks the elements of an array or string into individual values)---------------------  
 
 let numbers = [1,2,3,4,5];
 // let maximum = Math.max(numbers) // NAN but if we use spread with spread ...
 
-let maximum = Math.max(...numbers)
-console.log(maximum)
+/*
+let maximum = Math.max(...numbers) // unpacks numbers into individual arguments for Math.max, which then returns the maximum value among them.
+let minimum = Math.min(...numbers)
+console.log(maximum) 
+console.log(minimum) 
+*/
+console.log(Math.max(...numbers))
+
+let username = "Luna Drite";
+let letters = [...username] 
+
+console.log(letters.join(''))
